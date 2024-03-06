440674f960ec91c845c87707ec11a98fa8a8af5e
Marko Kojic <kojicmarko96@gmail.com>
2024-03-06 12:22:25 +0100

    fix: Added env vars to entrypoint

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.documents import router as documents_router
+from src.logos import router as logos_router
 from src.projects import router as projects_router
 from src.users import router as auth_router
 from src.utils.logger.main import setup_logging
@@ -13,6 +14,7 @@ app = FastAPI()
 app.include_router(projects_router.router)
 app.include_router(auth_router.router)
 app.include_router(documents_router.router)
+app.include_router(logos_router.router)
 
 
 @app.get("/health")
